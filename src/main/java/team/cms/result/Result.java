57810909a10f3cc2d373623fe3d60db503ecc49c
WHF <2281774687@qq.com>
2020-12-10 11:40:44 +0800

    merge调整

--- src/main/java/team/cms/result/Result.java
+++ src/main/java/team/cms/result/Result.java
@@ -5,13 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-<<<<<<< HEAD
 @AllArgsConstructor
 @NoArgsConstructor
-=======
-@NoArgsConstructor
-@AllArgsConstructor
->>>>>>> feature/user/message
 public class Result {
 
     private boolean success;
