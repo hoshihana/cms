6a8a25452e18b1c818c22c8d3def5cc822ca8c8a
WJX <6109118201@email.ncu.edu.cn>
2020-12-10 08:42:50 +0800

    新增Result基类

---
+++ src/main/java/team/cms/result/Result.java
@@ -0,0 +1,15 @@
+package team.cms.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+
+    private boolean success;
+
+    private String message;
+}
