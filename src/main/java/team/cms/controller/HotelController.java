cdd327f2e7d8b16a458e6e5efb152b3ac8a61957
WHF <2281774687@qq.com>
2020-12-12 16:24:07 +0800

    hotel信息查询，修改等功能，controller层完成

--- src/main/java/team/cms/controller/HotelController.java
+++ src/main/java/team/cms/controller/HotelController.java
@@ -1,4 +1,28 @@
 package team.cms.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.cms.entity.Hotel;
+import team.cms.service.HotelService;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/hotel")
 public class HotelController {
+
+    @Resource
+    HotelService hotelService;
+
+    @PostMapping("/getAll")
+    List<Hotel> getAll(){
+        return hotelService.getAll();
+    }
+
+    @PostMapping("/get")
+    Hotel getHotelById(Integer id) {
+        return hotelService.getHotelById(id);
+    }
 }
--- src/main/java/team/cms/controller/HotelProfileController.java
+++ src/main/java/team/cms/controller/HotelProfileController.java
@@ -1,4 +1,38 @@
 package team.cms.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.cms.entity.Hotel;
+import team.cms.result.Result;
+import team.cms.service.HotelService;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/hotel/profile")
 public class HotelProfileController {
+
+    @Resource
+    HotelService hotelService;
+
+    @PostMapping("")
+    Hotel getHotelProfile(HttpServletRequest request) {
+        Integer accountId = (Integer)request.getAttribute("accountId");
+        return hotelService.getHotelByAccountId(accountId);
+    }
+
+    @PostMapping("/modify")
+    Result modifyHotel(HttpServletRequest request,String name,String address,String detail,String telephone) {
+        Integer accountId = (Integer)request.getAttribute("accountId");
+        Hotel hotel=new Hotel();
+        hotel.setAccountId(accountId);
+        hotel.setName(name);
+        hotel.setDetail(detail);
+        hotel.setAddress(address);
+        hotel.setTelephone(telephone);
+        hotelService.modifyHotel(hotel);
+        return new Result(true,null);
+    }
 }
