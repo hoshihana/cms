fdf6af2d8982f4feefb87227717751bbd649c43f
WHF <2281774687@qq.com>
2020-12-12 15:20:17 +0800

    fleet接口功能完成

--- src/main/java/team/cms/controller/FleetController.java
+++ src/main/java/team/cms/controller/FleetController.java
@@ -1,10 +1,30 @@
 package team.cms.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.cms.entity.Fleet;
+import team.cms.service.FleetService;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/fleet")
 public class FleetController {
-    
+
+    @Resource
+    FleetService fleetService;
+
+    @PostMapping("/getAll")
+    List<Fleet> getAll()
+    {
+        return fleetService.getAll();
+    }
+
+    @PostMapping("/get")
+    Fleet getFleetById(Integer id)
+    {
+        return fleetService.getFleetById(id);
+    }
 }
