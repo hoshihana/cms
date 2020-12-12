2b7e371c3092aae4d69f014396e512e8a00f85dc
WHF <2281774687@qq.com>
2020-12-12 16:01:35 +0800

    hotel密码修改功能

---
+++ src/main/java/team/cms/controller/HotelPasswordController.java
@@ -0,0 +1,34 @@
+package team.cms.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.cms.entity.Account;
+import team.cms.result.PasswordModifyResult;
+import team.cms.service.AccountService;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/hotel/password")
+public class HotelPasswordController {
+
+    @Resource
+    AccountService accountService;
+
+    @PostMapping("/modify")
+    PasswordModifyResult updatePassword(HttpServletRequest request, String oldPassword, String newPassword)
+    {
+        Integer accountId = (Integer)request.getAttribute("accountId");
+        if(!accountService.checkPassword(accountId, oldPassword)) {
+            return new PasswordModifyResult(false, false, "旧密码错误");
+        } else {
+            Account account = new Account();
+            account.setId(accountId);
+            account.setPassword(newPassword);
+            accountService.modifyPassword(account);
+            return new PasswordModifyResult(true, true, null);
+        }
+    }
+}
