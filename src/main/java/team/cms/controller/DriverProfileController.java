b9f2d68fc0bad9c68358bc175dbc057c36500c1f
WHF <2281774687@qq.com>
2020-12-12 14:34:36 +0800

    通过车队id查找drivers信息接口

--- src/main/java/team/cms/controller/DriverProfileController.java
+++ src/main/java/team/cms/controller/DriverProfileController.java
@@ -1,10 +1,25 @@
 package team.cms.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.cms.entity.Driver;
+import team.cms.service.DriverService;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/driver/profile")
 public class DriverProfileController {
 
+    @Resource
+    DriverService driverService;
+
+    @PostMapping("/getByFleetId")
+    List<Driver> getFleetDrivers(Integer fleetId)
+    {
+        List<Driver> drivers= driverService.getFleetDrivers(fleetId);
+        return drivers;
+    }
 }
