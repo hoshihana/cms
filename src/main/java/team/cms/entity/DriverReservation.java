7f61f2781b939117da65e5985c1860d534fb3e7a
LiJin <963050074@qq.com>
2020-12-12 11:55:23 +0800

    修改数据库表结构，更新对应实体

---
+++ src/main/java/team/cms/entity/DriverReservation.java
@@ -0,0 +1,31 @@
+package team.cms.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DriverReservation {
+
+    private Integer hotelId;
+
+    private Integer conferenceId;
+
+    private Integer userId;
+
+    private Date reserveTime;
+
+    private boolean hotelCheck;
+
+    private Date checkinTime;
+
+    private String roomNumber;
+
+    private boolean userCheck;
+
+}
