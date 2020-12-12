195a3200f12050a292402ea846586d646b63f09e
WHF <2281774687@qq.com>
2020-12-12 15:03:30 +0800

    修改接口内容，divercontroller提供给用户查询司机信息， DriverProfileController用于给司机自己查看并修改自己信息

--- src/main/java/team/cms/controller/DriverProfileController.java
+++ src/main/java/team/cms/controller/DriverProfileController.java
@@ -3,10 +3,14 @@ package team.cms.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.cms.entity.Account;
 import team.cms.entity.Driver;
+import team.cms.entity.enums.Gender;
+import team.cms.result.Result;
 import team.cms.service.DriverService;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -16,10 +20,22 @@ public class DriverProfileController {
     @Resource
     DriverService driverService;
 
-    @PostMapping("/getByFleetId")
-    List<Driver> getFleetDrivers(Integer fleetId)
-    {
-        List<Driver> drivers= driverService.getFleetDrivers(fleetId);
-        return drivers;
+    @PostMapping("")
+    Driver getDriverProfile(HttpServletRequest request) {
+        Integer accountId = (Integer) request.getAttribute("accountId");
+        return driverService.getDriverByAccountId(accountId);
+    }
+
+    @PostMapping("/modify")
+    Result modifyDriverMessage(HttpServletRequest request, String name, Gender gender, String residentIdNumber, String telephone) {
+        Integer accountId = (Integer) request.getAttribute("accountId");
+        Driver driver = new Driver();
+        driver.setAccountId(accountId);
+        driver.setName(name);
+        driver.setResidentIdNumber(residentIdNumber);
+        driver.setTelephone(telephone);
+        driver.setGender(gender);
+        driverService.modifyDriver(driver);
+        return new Result(true, null);
     }
 }
