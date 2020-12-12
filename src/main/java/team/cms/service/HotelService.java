5e5e626f7cd9842a94fad67b25bb3fbd0f45b186
WHF <2281774687@qq.com>
2020-12-12 15:53:44 +0800

    hotel页面，service层实现

--- src/main/java/team/cms/service/HotelService.java
+++ src/main/java/team/cms/service/HotelService.java
@@ -2,7 +2,15 @@ package team.cms.service;
 
 import team.cms.entity.Hotel;
 
+import java.util.List;
+
 public interface HotelService {
 
-    Hotel getHotelByAccountId(int accountId);
+    Hotel getHotelByAccountId(Integer accountId);
+
+    List<Hotel> getAll();
+
+    Hotel getHotelById(Integer id);
+
+    void modifyHotel(Hotel hotel);
 }
--- src/main/java/team/cms/service/impl/HotelServiceImpl.java
+++ src/main/java/team/cms/service/impl/HotelServiceImpl.java
@@ -6,6 +6,7 @@ import team.cms.repository.HotelRepository;
 import team.cms.service.HotelService;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class HotelServiceImpl implements HotelService {
@@ -14,7 +15,22 @@ public class HotelServiceImpl implements HotelService {
     HotelRepository hotelRepository;
 
     @Override
-    public Hotel getHotelByAccountId(int accountId) {
+    public Hotel getHotelByAccountId(Integer accountId) {
         return hotelRepository.getHotelByAccountId(accountId);
+    }
+
+    @Override
+    public List<Hotel> getAll() {
+        return hotelRepository.getAll();
+    }
+
+    @Override
+    public Hotel getHotelById(Integer id) {
+        return hotelRepository.getHotelById(id);
+    }
+
+    @Override
+    public void modifyHotel(Hotel hotel) {
+        hotelRepository.modifyHotel(hotel);
     }
 }
