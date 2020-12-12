d6b8be0546b96ff7d5be9a429191482170d752a4
WHF <2281774687@qq.com>
2020-12-12 14:15:05 +0800

    driver页面的service层及dao层

--- src/main/java/team/cms/service/DriverService.java
+++ src/main/java/team/cms/service/DriverService.java
@@ -3,7 +3,15 @@ package team.cms.service;
 import org.springframework.stereotype.Service;
 import team.cms.entity.Driver;
 
+import java.util.List;
+
 public interface DriverService {
 
-    Driver getDriverByAccountId(int accountId);
+    List<Driver> getFleetDrivers(Integer fleetId);
+
+    Driver getDriverByAccountId(Integer accountId);
+
+    Driver getDriverById(Integer id);
+
+    void modifyDriver(Driver driver);
 }
