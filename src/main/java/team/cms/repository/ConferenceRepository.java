4bb637b27bccd2cf5d3b2a4cb9a53e994c5d7f50
WJX <6109118201@email.ncu.edu.cn>
2020-12-12 15:22:02 +0800

    会议repository层实现

--- src/main/java/team/cms/repository/ConferenceRepository.java
+++ src/main/java/team/cms/repository/ConferenceRepository.java
@@ -1,10 +1,39 @@
 package team.cms.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+import team.cms.entity.Conference;
+
+import java.util.Date;
 
 @Mapper
 @Repository
 public interface ConferenceRepository {
 
+    void updateProgress(Date currentTime);
+
+    Conference getOngoingAndCreatedConferenceByUserId(Integer userId);
+
+    Conference getOngoingAndParticipatedConferenceByUserId(Integer userId);
+
+    Conference getEndedAndCreatedConferenceByAccountUserId(Integer userId);
+
+    Conference getEndedAndParticipatedConferenceByUserId(Integer userId);
+
+    Conference getCreatedConferenceByUserIdAndId(@Param("userId") Integer userId, @Param("id")Integer id);
+
+    Conference getCreatedConferenceByUserIdAndNumber(@Param("userId") Integer userId, @Param("number")String number);
+
+    Conference getParticipatedConferenceByUserIdAndId(@Param("userId") Integer userId, @Param("id")Integer id);
+
+    Conference getParticipatedCreateConferenceByUserIdAndNumber(@Param("userId") Integer userId, @Param("number")String number);
+
+    Conference getConferenceById(Integer id);
+
+    Conference getConferenceByNumber(String number);
+
+    Conference addConference(Conference conference);
+
+    Conference updateNumber(Integer id);
 }
