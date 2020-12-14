362db823a1b685a64483c570a46f3d6c0cdedd9e
LiJin <963050074@qq.com>
2020-12-15 03:11:00 +0800

    版本控制

--- src/main/java/team/cms/controller/ConferenceController.java
+++ src/main/java/team/cms/controller/ConferenceController.java
@@ -3,14 +3,15 @@ package team.cms.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import team.cms.entity.Conference;
-import team.cms.service.ConferenceService;
 import team.cms.entity.Enrollment;
+import team.cms.entity.HotelReservation;
 import team.cms.entity.User;
 import team.cms.result.CountResult;
 import team.cms.result.Result;
+import team.cms.service.ConferenceService;
 import team.cms.service.EnrollmentService;
+import team.cms.service.HotelReservationService;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -25,6 +26,9 @@ public class ConferenceController {
     @Resource
     EnrollmentService enrollmentService;
 
+    @Resource
+    HotelReservationService hotelReservationService;
+
     @PostMapping("/getById")
     Conference getConferenceById(Integer id) {
         return conferenceService.getConferenceById(id);
@@ -33,8 +37,9 @@ public class ConferenceController {
     @PostMapping("/getByNumber")
     Conference getConferenceByNumber(String number) {
         return conferenceService.getConferenceByNumber(number);
-
     }
+
+
     @PostMapping("/participant/count")
     public CountResult getNumberOfEnrollment(Integer id) {
         Integer amount=enrollmentService.getNumberOfEnrollment(id);
@@ -63,13 +68,31 @@ public class ConferenceController {
             return new Result(false, "删除失败！");
     }
 
-    @PostMapping("/chooseHotel")
-    public Result chooseHotel(Integer id, Integer hotelId){
-        if(conferenceService.setConferenceHotel(id, hotelId))
-            return new Result(true,null);
-        else
-            return new Result(false,"选择酒店失败");
+    @PostMapping("/hotelReservation")
+    public List<HotelReservation> getAllHotelReservations(Integer id){
+        return hotelReservationService.getAllHotelReservationsByConferenceId(id);
     }
 
-}
+    @PostMapping("/hotelReservation/get")
+    public HotelReservation getParticipantHotelReservation(Integer id, Integer userId){
+        return hotelReservationService.getParticipantHotelReservation(id, userId);
+    }
 
+    @PostMapping("/hotelReservation/checkAll")
+    public boolean isAllHotelReservationChecked(Integer id){
+        List<HotelReservation> hotelReservations=hotelReservationService.getAllHotelReservationsByConferenceId(id);
+        for(HotelReservation h:hotelReservations){
+            if(!h.isHotelCheck()) return false;
+        }
+        return true;
+    }
+
+    @PostMapping("/hotelReservation/check")
+    public boolean checkHotelReservation(Integer id){
+        List<HotelReservation> hotelReservations=hotelReservationService.getAllHotelReservationsByConferenceId(id);
+        for(HotelReservation h:hotelReservations){
+            if(!h.isHotelCheck()) return false;
+        }
+        return true;
+    }
+}
