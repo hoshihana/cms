86caff23f769663546851dd9af01a417c248c9cf
LiJin <963050074@qq.com>
2020-12-15 01:25:07 +0800

    HotelReservationRepository接口

---
+++ src/main/java/team/cms/repository/HotelReservationRepository.java
@@ -0,0 +1,9 @@
+package team.cms.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface HotelReservationRepository {
+}
