d75be8ad1666c01fe06503b9ccff91124d7d20e5
LiJin <963050074@qq.com>
2020-12-15 02:56:50 +0800

    增加获取某酒店全部已确定预约功能

--- src/main/java/team/cms/controller/HotelController.java
+++ src/main/java/team/cms/controller/HotelController.java
@@ -4,9 +4,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.cms.entity.Hotel;
+import team.cms.result.UserHotelReservation;
 import team.cms.service.HotelService;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
@@ -25,4 +28,29 @@ public class HotelController {
     Hotel getHotelById(Integer id) {
         return hotelService.getHotelById(id);
     }
+
+    @PostMapping("/reservation/unchecked")
+    List<UserHotelReservation> getAllUncheckedReservations(HttpServletRequest request){
+        Integer accountId= (Integer) request.getAttribute("accountId");
+        List<UserHotelReservation> list=hotelService.getAllReservationsByAccountId(accountId);
+        List<UserHotelReservation> results=new LinkedList<>();
+
+        for(UserHotelReservation reservation:list){
+            if(!reservation.isHotelCheck()&&!reservation.isUserCheck()) results.add(reservation);
+        }
+        return results;
+    }
+
+    @PostMapping("/reservation/checked")
+    List<UserHotelReservation> getAllCheckedReservations(HttpServletRequest request){
+        Integer accountId= (Integer) request.getAttribute("accountId");
+        List<UserHotelReservation> list=hotelService.getAllReservationsByAccountId(accountId);
+        List<UserHotelReservation> results=new LinkedList<>();
+
+        for(UserHotelReservation reservation:list){
+            if(reservation.isHotelCheck()&&!reservation.isUserCheck()) results.add(reservation);
+        }
+        return results;
+    }
+
 }
