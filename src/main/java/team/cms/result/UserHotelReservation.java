b783d5183b5b23b900a7348b8a45dd3ef9b14e34
LiJin <963050074@qq.com>
2020-12-15 02:54:49 +0800

    增加获取某酒店全部未处理订单功能

--- src/main/java/team/cms/result/UserHotelReservation.java
+++ src/main/java/team/cms/result/UserHotelReservation.java
@@ -1,10 +1,16 @@
 package team.cms.result;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import team.cms.entity.enums.Gender;
 
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserHotelReservation {
     private Integer conferenceId;
 
