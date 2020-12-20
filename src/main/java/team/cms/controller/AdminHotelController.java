3f57652f8448d248e46e162065cf551c7e0c428b
LiJin <963050074@qq.com>
2020-12-21 01:38:33 +0800

    增加AdminHotelController控制器

---
+++ src/main/java/team/cms/controller/AdminHotelController.java
@@ -0,0 +1,17 @@
+package team.cms.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/hotel")
+public class AdminHotelController {
+
+
+
+
+
+
+
+}
