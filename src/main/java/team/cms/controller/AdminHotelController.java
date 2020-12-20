34af4e048ae007aaeb9662e901635126582279bf
LiJin <963050074@qq.com>
2020-12-21 01:42:15 +0800

    增加管理员获取已注册酒店数功能

--- src/main/java/team/cms/controller/AdminHotelController.java
+++ src/main/java/team/cms/controller/AdminHotelController.java
@@ -1,12 +1,25 @@
 package team.cms.controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.cms.service.AccountService;
+import team.cms.service.HotelService;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/api/admin/hotel")
 public class AdminHotelController {
+
+    @Resource
+    private HotelService hotelService;
+
+    @PostMapping("/count")
+    public Integer getHotelNumber(){
+        return hotelService.getAll().size();
+    }
 
 
 
