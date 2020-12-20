72662463fea646778e5671000aa435aaa77fe4bf
LiJin <963050074@qq.com>
2020-12-21 02:41:51 +0800

    增加管理员获取所有酒店订单功能

--- src/main/java/team/cms/controller/AdminHotelController.java
+++ src/main/java/team/cms/controller/AdminHotelController.java
@@ -4,10 +4,14 @@ package team.cms.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.cms.entity.Hotel;
+import team.cms.result.AdminHotelResult;
 import team.cms.service.AccountService;
 import team.cms.service.HotelService;
 
 import javax.annotation.Resource;
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/hotel")
@@ -20,6 +24,24 @@ public class AdminHotelController {
     public Integer getHotelNumber(){
         return hotelService.getAll().size();
     }
+
+    @PostMapping("/getAll")
+    public List<AdminHotelResult> getAllHotel(){
+//        List<AdminHotelResult> resultList=new LinkedList<>();
+//        List<Hotel> tempList=hotelService.getAll();
+//
+//        for(Hotel hotel:tempList){
+//            resultList.add(new AdminHotelResult(hotel.getAccountId(),hotel.))
+//        }
+        return null;
+
+
+
+
+    }
+
+
+
 
 
 
